d7cea5e546bae8b47c80074d4e996e2386d6f1f0
123rafiuolajumoke@gmail.com <123rafiuolajumoke@gmail.com>
2026-06-26 20:08:44 +0100

    Add payments app with Flutterwave bank transfer flow

--- lasop/urls.py
+++ lasop/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('payments.urls')),  
     path('api/users/', include('users.urls')),
     path('api/courses/', include('courses.urls')),
     path('api/applications/', include('applications.urls')),
-    path('api/', include('payments.urls')),  
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
