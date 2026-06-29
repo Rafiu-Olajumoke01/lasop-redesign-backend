8ccb0f9e671f691d87eb3244354f421c8dc7aad4
123rafiuolajumoke@gmail.com <123rafiuolajumoke@gmail.com>
2026-06-29 09:09:40 +0100

    Switch from Flutterwave to Paystack for payments

--- lasop/urls.py
+++ lasop/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('payments.urls')),  
     path('api/users/', include('users.urls')),
     path('api/courses/', include('courses.urls')),
     path('api/applications/', include('applications.urls')),
+    path('api/', include('payments.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
