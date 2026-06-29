2158594110c84aefbcb3585321eefb6fc203ef6f
123rafiuolajumoke@gmail.com <123rafiuolajumoke@gmail.com>
2026-06-29 20:37:59 +0100

    Switch from Flutterwave to Paystack for payments

--- users/serializers.py
+++ users/serializers.py
@@ -37,4 +37,4 @@ class LoginSerializer(serializers.Serializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'gender']
+        fields = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'gender', 'is_staff']
