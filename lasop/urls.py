8e393222bad1bcbb4254f47b95fa7eaa26177341
123rafiuolajumoke@gmail.com <123rafiuolajumoke@gmail.com>
2026-07-07 13:34:27 +0100

    Pin Apply button to bottom of mobile navbar drawer

--- lasop/urls.py
+++ lasop/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path('api/applications/', include('applications.urls')),
     path('api/cohorts/', include('cohorts.urls')),
     path('api/exams/', include('exams.urls')),
-    path('api/results/', include('exams.urls')),
     path('api/', include('payments.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
