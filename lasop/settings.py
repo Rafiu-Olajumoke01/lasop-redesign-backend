25d2a878025b78b020666c84ba59f6223382e638
123rafiuolajumoke@gmail.com <123rafiuolajumoke@gmail.com>
2026-09-04 21:55:12 +0100

    Use in-memory channel layer for now, defer Redis until scaling is needed

--- lasop/settings.py
+++ lasop/settings.py
@@ -91,10 +91,7 @@ DATABASES = {
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            'hosts': [os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379/0')],
-        },
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
     },
 }
 
