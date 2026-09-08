d404c74c670825510785d57fe869ca39b5f6727e
123rafiuolajumoke@gmail.com <123rafiuolajumoke@gmail.com>
2026-09-08 02:48:46 +0100

    fix: wire up ASGI WebSocket routing for chat

--- lasop/asgi.py
+++ lasop/asgi.py
@@ -1,16 +1,24 @@
 """
 ASGI config for lasop project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
 
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lasop.settings')
 
-application = get_asgi_application()
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chats.middleware import JWTAuthMiddleware
+import chats.routing
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": JWTAuthMiddleware(
+        URLRouter(
+            chats.routing.websocket_urlpatterns
+        )
+    ),
+})
