cabd8a97ab9e3986d6e96575318e45e5100fd903
123rafiuolajumoke@gmail.com <123rafiuolajumoke@gmail.com>
2026-09-15 15:28:19 +0100

    Fix email settings to read TLS/SSL/credentials from env vars

--- lasop/settings.py
+++ lasop/settings.py
@@ -193,9 +193,9 @@ STATIC_URL = 'static/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'localhost')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 25))
-EMAIL_USE_SSL = False
-EMAIL_USE_TLS = False
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-DEFAULT_FROM_EMAIL = 'no-reply@lasop.net'
+EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'False') == 'True'
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'False') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'no-reply@lasop.net')
 FRONTEND_URL = os.environ.get('FRONTEND_URL', 'https://lasop.net')
